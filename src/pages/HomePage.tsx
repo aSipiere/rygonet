bc92875caaceb278319b2048c659f2cd8714d5fc
Alex Sipiere <alex.sipiere@protonmail.com>
2025-12-29 01:17:59 +0000

    add notice board

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -57,6 +57,65 @@ export default function HomePage() {
 
       <Divider variant="bracketed" text="SYSTEM ACCESS" />
 
+      {/* System Administrator's Notice */}
+      <Box sx={{ my: 4 }}>
+        <TerminalBox title="NOTICE BOARD" variant="heavy">
+          <Typography
+            variant="body2"
+            sx={{
+              color: 'warning.main',
+              fontFamily: 'monospace',
+              fontWeight: 'bold',
+              mb: 1,
+            }}
+          >
+            29/12/1985 0100 GMT (Grotehaven Mean Time) - System Administrator's Notice:
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{
+              color: 'text.primary',
+              fontFamily: 'monospace',
+              lineHeight: 1.6,
+            }}
+          >
+            The new STANDARD RECOGNITION AND UNIT I.D. DATABASE is now live. For now, the prototype
+            is operational with BLUEFOR effectiveness statistics compiled and verified. Over the
+            coming days, profiles for neer peer adversaries will be compiled and uploaded to the system.
+            <br />
+            <br />
+            We've also improved the ability to model embarked, desanting and towed units when you
+            have multiple carriers, when you hover over a unit in the drop down it will highlight
+            which vehicle that selection pertains to.
+            <br />
+            <br />
+            If you notice any inaccuracies or peculiarities of the system, please notify a FEDINT
+            Liaison officer (@ahlakes on discord).
+            <br />
+            <br />
+            - SYSADMIN
+            
+          </Typography>
+          <Typography
+            variant="body2"
+            sx={{
+              color: 'warning.main',
+              fontFamily: 'monospace',
+              fontWeight: 'bold',
+              mb: 1,
+            }}
+          >
+            <br />
+            P.S.
+            Reports of innacurate unit or weaponry statistics will not be considered
+            until all near peer forces are added.
+            <br /> 
+            <br />
+            Thank you for your patience in this matter. 
+          </Typography>
+        </TerminalBox>
+      </Box>
+
       <Box sx={{ textAlign: 'center', my: 4 }}>
         <Button
           variant="contained"
